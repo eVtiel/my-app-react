62f42f465876a525d2b0b5ee72d07bee27187a8a
Oleksandr Shapovalov <evtiel@Oleksandrs-MBP.lan1>
2020-06-07 16:50:55 -0400

    created new project my-app

---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './post-status-filter.css'
+
+const PostStatusFilter = () => {
+    return (
+        <div className="btn-group">
+            <button type="button" className='btn btn-info'>Add</button>
+            <button type="button" className='btn btn-outline-secondary'>Liked</button>
+        </div>
+    )
+}
+
+export default PostStatusFilter;
