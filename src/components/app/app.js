d61dc0a98d4fd8cdfff2469dfa15db9f79041f7d
Oleksandr Shapovalov <evtiel@Oleksandrs-MacBook-Pro.local>
2020-06-08 21:42:54 -0400

    Added dynamic posts and event like and important

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: "Going to learn React", important: true, id: "dsafq"},
+        {label: "That is so good", important: false, id: "wewew"},
+        {label: "I need a break...", important: false, id: "rere"},
+    ];
+
     return (
        <div className="app">
             <AppHeader/>
@@ -16,7 +23,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
        </div>
     )
